d28e6ac7d4963c28c21aa4bde9a6ec2873709084
E6136 <metabloetax@gmail.com>
2022-03-08 18:12:52 +0200

    pick a winner

--- Lottery.sol
+++ Lottery.sol
@@ -5,12 +5,16 @@ import "./IERC20.sol";
 import "./ILendingPool.sol";
 
 contract Lottery {
+
+	event Winner(address winner);
+
 	// the timestamp of the drawing event
 	uint public drawing;
 	// the price of the ticket in DAI (100 DAI)
 	uint ticketPrice = 100e18;
 	// the user have already purchased a ticket?
 	mapping(address => bool) public hasTicket;
+	address[] usersAddresses;
 
 	ILendingPool pool = ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
 	IERC20 aDai = IERC20(0x028171bCA77440897B824Ca71D1c56caC55b68A3); 
@@ -29,16 +33,21 @@ contract Lottery {
 		require(hasTicket[msg.sender] == false, "You have already purchased a ticket.");
 		dai.transferFrom(msg.sender, address(this), ticketPrice);
 		hasTicket[msg.sender] = true;
-
-        /*As soon as a ticket is purchased, we can start earning interest on it for the lottery 
+		usersAddresses.push(msg.sender);
+		
+		
+		/*As soon as a ticket is purchased, we can start earning interest on it for the lottery 
 		depositing the DAI transferred from the user's account into the AAVE pool.*/
 		dai.approve(address(pool), ticketPrice);
 		pool.deposit(address(dai), ticketPrice, address(this), 0);
 	}
 
-	event Winner(address);
-
 	function pickWinner() external {
-        
+		require(block.timestamp >= drawing, "You need to wait.");
+
+		/*Using the blockhash of the previus block and the balance of the AAVE dai pool 
+		as sources of randomness to pick the winner from the array of users addresses*/
+		uint winner = (uint(blockhash(block.number - 1)) * dai.balanceOf(address(pool))) % usersAddresses.length;
+		emit Winner(usersAddresses[winner]);
 	}
 }
